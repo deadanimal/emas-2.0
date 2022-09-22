8ef792c0d93ef92c0398063efe0377755af6e4ce
adwapipeline <adwa.najmi@pipeline-network.com>
2022-09-22 15:32:20 +0800

    add pdf export button

--- resources/views/KT/kemasukanData/index.blade.php
+++ resources/views/KT/kemasukanData/index.blade.php
@@ -1,5 +1,7 @@
 @extends('base')
 @section('content')
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
+
     <div class="container">
         <div class="mb-4 text-center">
             <H2>PELAKSANAAN PROGRAM PEMBASMIAN
@@ -75,7 +77,7 @@
 
         </div>
 
-        <script>
+        {{-- <script>
             $(document).ready(function() {
                 $('#example').DataTable();
             });
@@ -95,6 +97,30 @@
                     },
 
                 }
+            });
+        </script> --}}
+
+        <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+        <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+
+        <script src="https://cdn.datatables.net/buttons/1.6.4/js/dataTables.buttons.min.js"></script>
+        <script src="https://cdn.datatables.net/buttons/1.6.4/js/buttons.html5.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+        <script src="https://cdn.datatables.net/buttons/1.6.4/js/buttons.html5.min.js"></script>
+
+        <script>
+            $(document).ready(function() {
+                $('#example').DataTable({
+                    dom: 'Bfrtip',
+                    buttons: [
+                        'copyHtml5',
+                        'excelHtml5',
+                        'csvHtml5',
+                        'pdfHtml5'
+                    ]
+                });
             });
         </script>
     @endsection

c0bcfd1fa842b6758e300943bb3d3b4ca15b9a72
Afeez Aziz <afeezaziz@gmail.com>
2022-11-23 11:45:37 +0800

    321

--- app/Http/Controllers/FokusutamaController.php
+++ app/Http/Controllers/FokusutamaController.php
@@ -47,7 +47,6 @@ class FokusutamaController extends Controller
 
         // $fokusutama = Fokusutama::all();
         // $role = Role::all();
-        dd($permissions = Auth::user()->permissions);
 
         $dalaman = Auth::user()->dalaman;
         if ($dalaman == 1) {

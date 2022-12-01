862014a607ccbef0ea88e31cb3bb389d9d77e0f9
Afeez Aziz <afeezaziz@gmail.com>
2022-12-01 21:38:08 +0800

    321

--- app/Http/Controllers/IntegrasiController.php
+++ app/Http/Controllers/IntegrasiController.php
@@ -26,7 +26,7 @@ class IntegrasiController extends Controller
         } else if($request->jenis == 'mukimdaerah') {
             $url = 'https://spkpn.epu.gov.my/api_spkpn/emas2_mukimdaerah/read.php?key=fc7fccd39e45a6993be6d38b997e886e';
         }
-        $response = Http::get($url)->json();
+        $response = Http::get($url, ['verify' => false])->json();
         return view('KT.integrasi_result', compact('response'));
     }
 }
